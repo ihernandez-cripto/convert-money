4a4c870b717655f45acf1025565325813263b2ee
Ivan Hernandez <irhernandezl@gmail.com>
2024-09-27 17:15:37 -0500

    display menu

---
+++ src/convertMoney.java
@@ -0,0 +1,10 @@
+public class convertMoney {
+    public static void main(String[] args) {
+        String menu = """
+                ******* Bienvenido al conversor de Moneda *******
+                1. Peso Colombiano (PCO) a Dolar (US)
+                7. Salir
+                """;
+
+    }
+}
